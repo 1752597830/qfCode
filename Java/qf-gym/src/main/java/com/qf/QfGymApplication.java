0346bfe11316b53d76d8f44c61484a3f085ee95c
清风 <1752597830@qq.com>
2023-01-02 14:03:10 +0800

    spring boot预约项目

---
+++ Java/qf-gym/src/main/java/com/qf/QfGymApplication.java
@@ -0,0 +1,18 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: sin
+ * @Date: 2023/1/2 - 01 - 02 - 13:37
+ * @Description: com.qf
+ * @version: 1.0
+ */
+
+@SpringBootApplication
+public class QfGymApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QfGymApplication.class, args);
+    }
+}
---
+++ Java/qf-gym/src/main/java/com/qf/pojo/User.java
@@ -0,0 +1,10 @@
+package com.qf.pojo;
+
+/**
+ * @Auther: sin
+ * @Date: 2023/1/2 - 01 - 02 - 13:37
+ * @Description: com.qf.pojo
+ * @version: 1.0
+ */
+public class User {
+}
